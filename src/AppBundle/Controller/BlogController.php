5a72bf6bf7a3b03a130b63879241ff9d94cbaa1b
Fabien Potencier <fabien.potencier@gmail.com>
2015-05-03 22:28:49 +0200

    optimized things

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -37,8 +37,6 @@ class BlogController extends Controller
      */
     public function indexAction()
     {
-        sleep(1);
-
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository('AppBundle:Post')->findLatest();
 
