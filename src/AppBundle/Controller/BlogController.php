623f31871b872be7ea79b6d6efbd53f57dafe825
Fabien Potencier <fabien.potencier@gmail.com>
2015-05-03 23:26:32 +0200

    new master

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -37,8 +37,6 @@ class BlogController extends Controller
      */
     public function indexAction()
     {
-        // noop
-
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository('AppBundle:Post')->findLatest();
 
